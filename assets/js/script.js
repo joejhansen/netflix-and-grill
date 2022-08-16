230f680a6bf08f52789589c81fba877804840002
joejhansen <josephjameshansen@gmail.com>
2022-08-16 10:11:55 -0700

    cleaning up javascript and adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// declaring variables
 var row1Vanilla = $("#row1")
 var row2 = $("#row2")
 var btnProfile1 = $("#btnProfile1")
@@ -5,28 +6,36 @@ var btnProfile2 = $("btnProfile2")
 var row1Vanilla = document.getElementById("row1")
 var row2Vanilla = document.getElementById("row2")
 var counter = 0
-var movieInfo = []
+var movieInfo
 var foodInfo = []
 
-function goProfile1() {
-    console.log(row1Vanilla.firstChild)
+// removing stuff in the DOM first and second row
+function removeSplash() {
     while (row1Vanilla.firstChild) {
         row1Vanilla.removeChild(row1Vanilla.firstChild)
     }
     while (row2Vanilla.firstChild) {
         row2Vanilla.removeChild(row2Vanilla.firstChild)
     }
+}
+
+// fetching APIS [DO MOVIES THEN FOOD]
+function initFetches() {
     fetch("https://imdb-api.com/en/API/MostPopularMovies/k_8wpck7yy")
         .then(function (response) {
             return response.json();
         })
-        .then(data => changeImageMovie(data))
+        // use this format to load data in from the food api through a function
+        .then(data => loadMovies(data))
+        // has to be done strictly after the data has loaded or else we get an error
+        .then(displayMoviePosters)
 }
-btnProfile1.on('click', goProfile1)
 
-function changeImageMovie(data) {
-    // console.log(data)
-    for (let i=0; i < 9; i++) {
+// loads up the movies into a global array
+function loadMovies(data) {
+    console.log(data)
+    movieInfo = []
+    for (let i = 0; i < 9; i++) {
         var thisMovie = {
             position: i,
             image: data.items[i].image,
@@ -35,6 +44,10 @@ function changeImageMovie(data) {
         }
         movieInfo.push(thisMovie)
     }
+}
+
+// displays the current movie. Should fine for the first one but we need to change the variable for which numbered thing in the array we want
+function displayMoviePosters() {
     console.log(movieInfo)
     var movieDisplay = document.createElement("img")
     movieDisplay.setAttribute('src', movieInfo[0].image)
@@ -45,6 +58,16 @@ function changeImageMovie(data) {
     row2Vanilla.innerHTML = '<a data-decision="dislike" class="waves-effect waves-light btn"><i data-decision="dislike" class="material-icons right">thumb_down</i>Dislike</a><a data-decision="like" class="waves-effect waves-light btn"><i data-decision="like" class="material-icons right">thumb_up</i>Like</a>'
 }
 
+// the initial call for profile 1
+function goProfile1() {
+    removeSplash();
+    initFetches()
+}
+
+// click event listener
+btnProfile1.on('click', goProfile1)
+
+// functionality for the like and dislike buttons
 function decisionMadeMovie(event) {
     console.log(event.target)
     if (event.target.getAttribute('data-decision') === "like") {
